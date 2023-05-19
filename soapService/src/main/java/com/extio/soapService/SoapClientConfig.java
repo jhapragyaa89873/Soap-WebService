7c50dd563d80bb6649d5263fa552d0dc9028f28e
jhapragyaa89873 <133594971+jhapragyaa89873@users.noreply.github.com>
2023-05-19 12:21:24 +0530

    Added Code comment

--- soapService/src/main/java/com/extio/soapService/SoapClientConfig.java
+++ soapService/src/main/java/com/extio/soapService/SoapClientConfig.java
@@ -9,11 +9,14 @@ import com.extio.service.TemperatureConversionService;
 
 @Configuration
 public class SoapClientConfig {
+	//constants for the required urls or we can directly use from application.poperties
     private static final String TEMPERATURE_WSDL_URL = "https://www.w3schools.com/xml/tempconvert.asmx?WSDL";
     private static final String NUMBER_TO_WORD_WSDL_URL = "https://www.dataaccess.com/webservicesserver/NumberConversion.wso?WSDL";
 
+    
     @Bean
     public TemperatureConversionService temperatureConversionService() {
+    	//using inbuilt factory bean for setting the values from service interface
         JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
         factory.setServiceClass(TemperatureConversionService.class);
         factory.setAddress(TEMPERATURE_WSDL_URL);
@@ -22,6 +25,7 @@ public class SoapClientConfig {
 
     @Bean
     public NumberToWordConversionService numberToWordConversionService() {
+    	//using inbuilt factory bean for setting the values from service interface
         JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
         factory.setServiceClass(NumberToWordConversionService.class);
         factory.setAddress(NUMBER_TO_WORD_WSDL_URL);
--- soapService/src/main/java/com/extio/soapService/SoapServiceApplication.java
+++ soapService/src/main/java/com/extio/soapService/SoapServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SoapServiceApplication {
     public static void main(String[] args) {
+    	//main class for executing the project
         SpringApplication.run(SoapServiceApplication.class, args);
     }
 }
